980b05501c9b739e04e5800eb504df053baf1424
H4t1ng <128522299+H4t1ng@users.noreply.github.com>
2025-06-14 15:32:17 -0300

    Mudança de cores

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ function App() {
 
   return (
     <div className="flex h-screen justify-center items-center">
-      <div className="card w-[80%] lg:w-[50%] h-[70%] shadow-md rounded-sm transform ease-out duration-300 items-center p-10 gap-5">
+      <div className="card w-[80%] lg:w-[50%] h-[70%] shadow-md rounded-sm transform ease-out duration-300 items-center p-10 gap-5 bg-white">
         <h1 className="text-5xl font-work font-bold w-fit text-center">
           Quests To Do
         </h1>
